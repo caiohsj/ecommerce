16f0ac34b66359c1c62a6d89a77e24f4078fa378
caiohsj <caiocoxim10@gmail.com>
2019-02-11 15:15:38 -0300

    Criado tela login

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -18,12 +19,40 @@ $app->get('/', function() {
 
 });
 
+//Rota para página inicial do admin
 $app->get('/admin', function() {
 	
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
+});
+
+//Rota para a página de login do admin
+$app->get('/admin/login', function(){
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+]);
+
+	$page->setTpl("login");
+});
+
+//Rota
+$app->post('/admin/login', function(){
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function(){
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 });
 
 $app->run();
